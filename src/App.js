dc564ee555b5f775672a68d8e960baab1b39e877
Débora Bucci <dbucci@protonmail.com>
2022-01-23 14:22:34 -0300

    delete the 'users' array

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
 function App() {
-  const users = [
-    {
-      name: "Deb",
-      age: "22",
-    },
-  ];
-
   return (
     <div>
       <AddUser />
-      <UserList users={users} />
+      <UserList />
     </div>
   );
 }
