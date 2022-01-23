739e442c59efcb06eb50f8dd652a103ea023c1dc
Débora Bucci <dbucci@protonmail.com>
2022-01-23 14:10:16 -0300

    add the 'Card' component wrapping the <ul> element

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./UserList.module.css";
 
 const UserList = ({ users }) => {
   return (
-    <ul>
-      {users.map(({ name, age }) => (
-        <li>
-          {name} ({age} years old)
-        </li>
-      ))}
-    </ul>
+    <Card>
+      <ul>
+        {users.map(({ name, age }) => (
+          <li>
+            {name} ({age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
   );
 };
 
