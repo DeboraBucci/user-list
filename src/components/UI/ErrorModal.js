28fa313f23d760a83c467231672fab6049c82a2d
Debora Bucci <dbucci@protonmail.com>
2022-01-25 10:26:54 -0300

    separate the content of the <ErrorModa> component into two different functions, and then inside <React.Fragment> create two portals for each function and link them to their respective roots

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,22 +2,39 @@ import React from "react";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = ({ onConfirm }) => {
+  return <div className={styles.backdrop} onClick={onConfirm}></div>;
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onConfirm}>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <React.Fragment>
-      <div className={styles.backdrop} onClick={onConfirm}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onConfirm}>Ok</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
